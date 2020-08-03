c1d11a943a34e4f76044aa366c8465fea21ee605
wamu2 <wamu@retro11.de>
2020-08-03 15:55:27 +0200

    add msgpack cpp

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-This project is part of the DockerHub image build setup for tests of
-the CBM DAQ CI/CD pipelines under GitLab.
+This project is part of the [DockerHub image](https://hub.docker.com/u/wfjm)
+build setup for tests of the CBM DAQ CI/CD pipelines under GitLab.
 
 It creates an image with all required packages for Ubuntu Focal (20.04 LTS)
 based tests.
--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ set -o pipefail
 set -e
 set -x
 
+# define versions
+export VTAG_GTEST=tags/release-1.10.0
+export VTAG_MSGPACK_CPP=70912ff
+
 # OS version info
 grep VERSION /etc/os-release
 
@@ -11,11 +15,21 @@ grep VERSION /etc/os-release
 cd /tmp
 git clone https://github.com/google/googletest.git
 cd googletest
+git checkout $VTAG_GTEST
 mkdir build
 cd build
 cmake ..
 make
 make install
+
+# install msgpack cpp
+cd /tmp
+git clone https://github.com/msgpack/msgpack-c.git
+cd msgpack-c
+git checkout cpp_master
+git checkout $VTAG_MSGPACK_CPP
+cd include
+cp -a * /usr/local/include
 
 # drop tmp files
 cd ..
